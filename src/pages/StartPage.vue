d2d9a6a4e804f19d918b1ea7e7b07bb5ce74ba5c
m-nick <sunty1@mail.ru>
2019-12-02 14:28:42 +0300

    Make StartPage

---
+++ src/pages/StartPage.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="start">
+    <div class="start__variant">
+      <file-loader></file-loader>
+    </div>
+    <div class="start__variant">
+      <button class="start__button">Создать</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import FileLoader from '@/components/FileLoader'
+
+export default {
+  name: 'StartPage',
+  components: {
+    FileLoader,
+  },
+  data() {
+    return {
+
+    };
+  },
+};
+</script>
+
+<style scoped lang="scss" src="@/styles/StartPage.scss">
+
+</style>
