d1a9eee7b17fa15012643541089a1613d022b4b6
m-nick <sunty1@mail.ru>
2019-12-02 14:27:45 +0300

    Add default styles

--- src/App.vue
+++ src/App.vue
@@ -5,5 +5,23 @@
 </template>
 
 <style lang="scss">
-
+  *,*::before,*::after{
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    color: unset;
+    text-decoration: none;
+    background: none;
+    border: none;
+    font-family: sans-serif;
+  }
+  .visually-hidden{
+    display: block;
+    position: absolute;
+    width: 1px;
+    height: 1px;
+    clip: rect(0 0 0 0);
+    overflow: hidden;
+    visibility: hidden;
+  }
 </style>
